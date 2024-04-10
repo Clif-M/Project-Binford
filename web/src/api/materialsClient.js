f740989633a8fa3f516d69cdf994bfdbf19e5ab9
aayers575 <132768307+aayers575@users.noreply.github.com>
2024-04-10 11:46:43 -0500

    Completed frontend functionality for materials (#71)

--- web/src/api/materialsClient.js
+++ web/src/api/materialsClient.js
@@ -10,16 +10,21 @@ import Authenticator from "./authenticator";
  * https://javascript.info/mixins
   */
 export default class MaterialsClient extends BindingClass {
+
     constructor(props = {}) {
         super();
-        const methodsToBind = ['clientLoaded', 'getIdentity', 'login', 'logout', 'editMaterial', 'addMaterial', 'deleteMaterial', 'getMaterials', 'search'];
+
+        const methodsToBind = ['clientLoaded', 'getIdentity', 'login', 'logout', 'editMaterial', 'addMaterial', 'deleteMaterial', 'getSingleMaterial', 'getMultipleMaterials'];
         this.bindClassMethods(methodsToBind, this);
+
         this.authenticator = new Authenticator();;
         this.props = props;
+
         axios.defaults.baseURL = process.env.API_BASE_URL;
         this.axiosClient = axios;
         this.clientLoaded();
     }
+
     /**
      * Run any functions that are supposed to be called once the client has loaded successfully.
      */
@@ -28,6 +33,7 @@ export default class MaterialsClient extends BindingClass {
             this.props.onReady(this);
         }
     }
+
     /**
      * Get the identity of the current user
      * @param errorCallback (Optional) A function to execute if the call fails.
@@ -36,46 +42,37 @@ export default class MaterialsClient extends BindingClass {
     async getIdentity(errorCallback) {
         try {
             const isLoggedIn = await this.authenticator.isUserLoggedIn();
+
             if (!isLoggedIn) {
                 return undefined;
             }
+
             return await this.authenticator.getCurrentUserInfo();
         } catch (error) {
             this.handleError(error, errorCallback)
         }
     }
+
     async login() {
         this.authenticator.login();
     }
+
     async logout() {
         this.authenticator.logout();
     }
+
+
     async getTokenOrThrow(unauthenticatedErrorMessage) {
         const isLoggedIn = await this.authenticator.isUserLoggedIn();
         if (!isLoggedIn) {
             throw new Error(unauthenticatedErrorMessage);
         }
+
         return await this.authenticator.getUserToken();
     }
     //---------------------------------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------------------------------------------------------------------------------------------
-    async getSingleMaterial(orgId, materialId, errorCallback) {
-        try {
-            const response = await this.axiosClient.get(`organizations/${orgId}/materials/${materialId}`);
-            return response.data.material;
-        } catch (error) {
-            this.handleError(error, errorCallback)
-        }
-    }
 
-    async getMaterials(orgId, errorCallback) {
-        try {
-            const response = await this.axiosClient.get(`organizations/${orgId}/materials`);
-            return response.data.projectList;
-        } catch (error) {
-            this.handleError(error, errorCallback)
-        }
-    }
     async editMaterial(orgId, materialId, cost, inventoryCount, isExpendable, name, errorCallback) {
         try {
             const token = await this.getTokenOrThrow("Only authenticated users can edit materials");
@@ -91,11 +88,14 @@ export default class MaterialsClient extends BindingClass {
                                 Authorization: `Bearer ${token}`
                             }
                         });
-            return response.data.materialList;
+          
+            return response.data.material;
+
         } catch (error) {
             this.handleError(error, errorCallback)
         }
     }
+
         async addMaterial(orgId, cost, inventoryCount, isExpendable, name, errorCallback) {
             try {
                 const token = await this.getTokenOrThrow("Only authenticated users can add materials");
@@ -110,11 +110,14 @@ export default class MaterialsClient extends BindingClass {
                                     Authorization: `Bearer ${token}`
                                 }
                             });
-                return response.data.materialList;
+
+                return response.data.material;
+
             } catch (error) {
                 this.handleError(error, errorCallback)
             }
         }
+
     async deleteMaterial(orgId, materialId, cost, inventoryCount, isExpendable, name, errorCallback) {
         try {
             const token = await this.getTokenOrThrow("Only authenticated users can edit materials");
@@ -126,28 +129,49 @@ export default class MaterialsClient extends BindingClass {
                                 Authorization: `Bearer ${token}`
                             }
                         });
-            return response.data.materialList;
+
+            return response.data.material;
         } catch (error) {
             this.handleError(error, errorCallback)
         }
     }
-    async search(criteria, errorCallback) {
-            try {
-                const queryParams = new URLSearchParams({ q: criteria })
-                const queryString = queryParams.toString();
-                const response = await this.axiosClient.get(`organizations/${queryString}/materials`);
-                return response.data.playlists;
-            } catch (error) {
-                this.handleError(error, errorCallback)
-            }
+
+    async getSingleMaterial(orgId, materialId, errorCallback) {
+        try {
+            const token = await this.getTokenOrThrow("Encountered token error trying to call Task endpoint.");
+            const response = await this.axiosClient.get(`organizations/${orgId}/materials/${materialId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }});
+            return response.data.material;
+
+        } catch (error) {
+            this.handleError(error, errorCallback)
         }
+    }
+
+    async getMultipleMaterials(orgId, errorCallback) {
+        try {
+            const token = await this.getTokenOrThrow("Encountered token error trying to call Material endpoint.");
+            const response = await this.axiosClient.get(`organizations/${orgId}/materials`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }});
+            return response.data.materials;
+        } catch (error) {
+            this.handleError(error, errorCallback)
+        }
+    }
+
     handleError(error, errorCallback) {
         console.error(error);
+
         const errorFromApi = error?.response?.data?.error_message;
         if (errorFromApi) {
             console.error(errorFromApi)
             error.message = errorFromApi;
         }
+
         if (errorCallback) {
             errorCallback(error);
         }
